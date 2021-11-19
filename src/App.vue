947881be6c6c317b3c7d6d08816065e37c8523aa
SokolArr <alex970899@gmail.com>
2021-11-19 15:19:58 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <MainWrapper />
   </div>
 </template>
 
+<script>
+import MainWrapper from "@/components/MainWrapper";
+
+export default {
+  name: "App",
+  components: {
+    MainWrapper,
+  },
+};
+</script>
+
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Play&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Exo+2:wght@100;200&family=Play&display=swap");
+
+html {
+  /*font-family: "Play", serif;*/
+  font-family: "Exo 2", sans-serif;
+}
+
+body {
+  background: #eeeeee;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100%;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  box-sizing: border-box;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Router from "@/router/router";
+import store from "./vuex/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
   render: (h) => h(App),
+  store: store,
+  router: Router,
 }).$mount("#app");
